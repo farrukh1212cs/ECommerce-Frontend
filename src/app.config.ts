bc0b9dc11ca60ed620434d7f070f795bab6ae181
Farrukh Rehman <farrukh1212cs@outlook.com>
2026-02-16 10:48:58 +0500

    check in

--- src/app.config.ts
+++ src/app.config.ts
@@ -6,13 +6,14 @@ import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
 import { authInterceptor } from '@/core/interceptors/auth.interceptor';
+import { errorInterceptor } from '@/core/interceptors/error.interceptor';
 
 
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
-        provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+        provideHttpClient(withFetch(), withInterceptors([authInterceptor, errorInterceptor])),
         provideAnimationsAsync(),
         providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
     ]
